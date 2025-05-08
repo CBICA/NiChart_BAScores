[bug] fixing r2 score computation on pairwise training | Added Literals for for type checking

BAScores/loader.py
@@ -9,6 +9,7 @@ import pandas as pd
 import torch
 import torchio as tio
 from torch.utils.data import DataLoader, Dataset
+from typing_extensions import Literal
 
 from BAScores.utils import get_prefix, get_prefix_oasis
 
@@ -35,7 +36,7 @@ class SingleSubjectDataloader(Dataset):
 
     def __init__(
         self,
-        mode: str,
+        mode: Literal["train", "evaluate", "inference"],
         in_dir: str,
         label_dict: Optional[dict] = None,
         data_augmentation: bool = False,
@@ -138,7 +139,7 @@ class PairwiseDataloader(Dataset):
 
     def __init__(
         self,
-        mode: str,
+        mode: Literal["train", "evaluate", "inference"],
         in_dir: str,
         label_dict: Optional[dict] = None,
         data_augmentation: bool = False,

BAScores/pairwise/train.py
@@ -5,6 +5,7 @@ import torch
 from torch.utils.data import DataLoader
 from torchmetrics import MeanSquaredError, NormalizedRootMeanSquaredError, R2Score
 from tqdm.auto import tqdm
+from typing_extensions import Literal
 
 from BAScores.pairwise.evaluate import evaluate
 from BAScores.utils import EarlyStopper
@@ -15,7 +16,7 @@ def train_step(
     dataloader: DataLoader,
     loss_fn: torch.nn.Module,
     optimizer: torch.optim.Optimizer,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
 ) -> Dict:
 
     model.train(True)
@@ -64,7 +65,7 @@ def test_step(
     model: torch.nn.Module,
     dataloader: DataLoader,
     loss_fn: torch.nn.Module,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
 ) -> Dict:
 
     # eval mode
@@ -91,7 +92,7 @@ def test_step(
             stats["test_mae"] += loss.item()
             mse.update(test_pred_logits, y1 - y2)
             nrmse.update(test_pred_logits, y1 - y2)
-            r2_score.udpate(test_pred_logits, y1 - y2)
+            r2_score.update(test_pred_logits, y1 - y2)
 
     stats["test_mae"] = stats["test_mae"] / float(len(dataloader))
     stats["test_mse"] = mse.compute().item()
@@ -110,7 +111,7 @@ def train(
     loss_fn: torch.nn.Module,
     epochs: int,
     patience: int,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
     target_dir: str = ".",
     model_name: str = "NiChart_BAScores_best.pth",
     verbose: bool = False,

BAScores/single_subject/evaluate.py
@@ -8,6 +8,7 @@ from torchmetrics import (
     NormalizedRootMeanSquaredError,
     R2Score,
 )
+from typing_extensions import Literal
 
 from BAScores.utils import load_single_model_weights, plot_preds_vs_truth
 
@@ -15,7 +16,7 @@ from BAScores.utils import load_single_model_weights, plot_preds_vs_truth
 def evaluate(
     model: torch.nn.Module,
     dataloader: DataLoader,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
     model_weights: Optional[str] = None,
     verbose: bool = False,
     plot_path: Optional[str] = None,

BAScores/single_subject/inference.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
+from typing_extensions import Literal
 
 from BAScores.loader import SingleSubjectDataloader
 from BAScores.utils import load_single_model_weights
@@ -14,7 +15,7 @@ def inference(
     in_dir: str,
     out_dir: str,
     csv_name: str,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
     batch_size: int = 16,
 ) -> None:
 

BAScores/single_subject/train.py
@@ -4,6 +4,7 @@ import torch
 from torch.utils.data import DataLoader
 from torchmetrics import MeanSquaredError, NormalizedRootMeanSquaredError, R2Score
 from tqdm.auto import tqdm
+from typing_extensions import Literal
 
 from BAScores.single_subject.evaluate import evaluate
 from BAScores.utils import EarlyStopper
@@ -14,7 +15,7 @@ def train_step(
     dataloader: DataLoader,
     loss_fn: torch.nn.Module,
     optimizer: torch.optim.Optimizer,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
 ) -> dict:
 
     model.train()
@@ -66,7 +67,7 @@ def test_step(
     model: torch.nn.Module,
     dataloader: DataLoader,
     loss_fn: torch.nn.Module,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
 ) -> dict:
 
     # eval mode
@@ -107,7 +108,7 @@ def train(
     loss_fn: torch.nn.Module,
     epochs: int,
     patience: int,
-    device: str,
+    device: Literal["cuda", "mps", "cpu"] = "cuda",
     target_dir: str = ".",
     model_name: str = "NiChart_BAScores_best.pth",
     verbose: bool = False,