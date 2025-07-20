1344dff234c139115e970855e96c188a6e7943c0
spirosmaggioros <spirosmag@ieee.org>
2025-07-20 16:48:31 -0400

    .

--- inference.sh
+++ inference.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=a100:1
-#SBATCH --cpus-per-gpu=16
-#SBATCH --mem-per-gpu=350GB
+#SBATCH --cpus-per-gpu=20
+#SBATCH --mem-per-gpu=500GB
 #SBATCH --output=log/inference.log
 #SBATCH --error=log/inference_error.log
 #SBATCH --partition=ailong
 #SBATCH --time=10:00:00
 
 NiChart_BAScores inference \
-                 --in_dir ../Datasets/BAScores/ISTAG_CN_All/istaging_2_new/BMI/BMI_PURE \
-                 --out_dir ../Datasets/BAScores/ISTAG_CN_All/istaging_2_new/attention_maps/BMI/BMI_PURE \
+                 --in_dir ../Datasets/BAScores/ISTAG_CN_All/istaging_2_new/hypertension/hypertension_PURE \
+                 --out_dir ../Datasets/BAScores/ISTAG_CN_All/istaging_2_new/attention_maps/hypertension/hypertension_PURE \
                  --csv ../test.csv \
                  --model resnet18 \
                  --mode regression \
